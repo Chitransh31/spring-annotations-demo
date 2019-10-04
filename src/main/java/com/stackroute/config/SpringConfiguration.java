57419fb103bc3a575a25011209e9ed2d3d85a69b
Chitransh31 <Saurabhbagade101@gmail.com>
2019-10-04 13:01:05 +0530

    "Annotations Task2 First Commit"

--- src/main/java/com/stackroute/config/SpringConfiguration.java
+++ src/main/java/com/stackroute/config/SpringConfiguration.java
@@ -7,16 +7,24 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.stackroute.domain")
 public class SpringConfiguration {
 
     @Bean
-    public Actor getActorBean() {
+    public Actor actor() {
         return new Actor("Keanu Reeves","Male",22);
     }
 
     @Bean
-    public Movie getMovieBean() {
-        return new Movie(getActorBean());
+    public Actor getActorBean2() {
+        return new Actor("Heath Ledger","Male",30);
     }
+
+    @Bean
+    public Actor getActorBean3() {
+        return new Actor("Jennifer Lawrence","Female",28);
+    }
+
+
 
 }
